fd6434be43a192445c0d79c736efaab8d3e1eb39
Bryce Tichit <bryce.tichit@u-psud.fr>
2016-05-02 17:05:44 +0200

    bloqué sur split_segment. C'est un gros bordel.

--- src/segment.ml
+++ src/segment.ml
@@ -14,12 +14,24 @@ type tpos = L | R | C
 
 let new_segment xo yo xd yd = let idc = idCount () in { id=idc ; porig=Point.new_point xo yo; pdest=Point.new_point xd yd; ci=0.0;ce=1.0 } 
 
-let get_position p s = 
-     let z = (s.pdest.x - s.porig.x) * (p.y - s.porig.y) - (s.pdest.y - s.porig.y) * (p.x - s.porig.x)
+let get_z p s = (s.pdest.x - s.porig.x) * (p.y - s.porig.y) - (s.pdest.y - s.porig.y) * (p.x - s.porig.x) 
+
+let get_position p s =
+     let z = get_z p s
      in if z > 0 then L
      else if z=0 then C
      else R;;
 
-let split_segment d s = failwith "TODO"
+let split_segment d1 s left = 
+    let d = (d1.pdest.x - d1.porig.x) * (s.pdest.y - s.porig.y) - (d1.pdest.y - 
+            d1.porig.y) * (s.pdest.x - s.porig.x)
+    in let r = ((d1.porig.y - s.porig.y) * (s.pdest.x - s.porig.x) - (d1.porig.x -
+                s.porig.c) * (s.pdest.y - s.porig.y)) / d
+    in let s = ((
+    in if d=0 || c < 0 || c >= 1 then if left then (Some s,None) else (None,Some s)
+    else if c > 0 && c < 1 then if left then (*séparer*) else (*séparer reverse*)
+    else if left then split_segment
+
+    
 
 let split hd rest = failwith "TODO"
