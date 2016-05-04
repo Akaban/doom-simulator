e44fc5a31bd5d8e6974ab7f3ff964664251b8544
bryce <bryce@within.redcloud.fr>
2016-05-04 21:56:33 +0200

    Ajout du module debug

---
+++ src/debug.ml
@@ -0,0 +1,27 @@
+open Point
+open Segment
+open Graphics
+open Printf
+open Player
+
+let drawSegment s =
+  let (xo,yo),(xd,yd) = real_coord s in
+  Graphics.draw_segments [|(truncate xo,truncate yo,truncate xd,truncate yd)|]
+
+let drawCollisionZone s =
+  if s.segBottom = None then () 
+  else let getSome = fromSome s in
+  let segs = [getSome s.segBottom; getSome s.segLeft; getSome s.segRight; getSome s.segTop]
+  in List.iter drawSegment segs
+
+let debugKeys k player bsp =
+    match k with
+      | 'c' -> printf "Affichage des  zones de collisions des segments\n"; flush stdout;
+               set_color red ; Bsp.iter drawCollisionZone bsp ; set_color black
+      | 'g' -> printf "Affichage de tout les segments dispo\n";flush stdout ;
+               Bsp.iter (fun s -> printf "%s\n" (toString s) ; flush stdout) bsp
+      | 'r' -> printf "Reset map\n" ; flush stdout ; clear_graph () ; Render.display bsp player ; synchronize ()
+      | 'p' -> printf "Les coordonnées du joueur sont %s avec un angle de %d\n" (Point.toString player.pos) player.pa ; flush stdout
+      | 'm' -> Options.debug := not !Options.debug ; if !Options.debug then printf "Option debug activated" else printf "Option debug disabled" ; flush stdout
+      | _ -> () 
+                
---
+++ src/debug.mli
@@ -0,0 +1,3 @@
+val drawSegment : Segment.t -> unit
+val drawCollisionZone : Segment.t -> unit
+val debugKeys : char -> Player.t -> Bsp.t -> unit
